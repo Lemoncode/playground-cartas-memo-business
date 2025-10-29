71b10ce4dd36a01cd102d94b78030418c222bf91
Braulio <braulio.diez@gmail.com>
2025-10-29 18:00:33 +0100

    00 barajar cartas

---
+++ 00-barajar-cartas/src/funcion.ts
@@ -0,0 +1,8 @@
+import type { Carta } from "./model";
+
+/*
+En el motor nos va a hacer falta un método para barajar cartas
+*/
+export const barajarCartas = (cartas: Carta[]) => {
+  return [];
+};
